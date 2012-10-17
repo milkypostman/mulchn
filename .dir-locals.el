4635465517b51cd759a3924deb61fb2cc1bf5d72
Donald Curtis <dcurtis@milkbox.net>
2012-10-16 22:47:44 -0500

    add .dir-locals.el for us emacs users

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+((nil . ((python-shell-virtualenv-path . "venv")
+         (python-shell-interpreter . "python")
+         (python-shell-prompt-regexp . ">>> ")
+         (python-shell-prompt-output-regexp . "")
+         (python-shell-completion-module-string-code . "")
+         (python-shell-completion-string-code . "';'.join(__COMPLETER_all_completions('''%s'''))
+"))))
+
+
