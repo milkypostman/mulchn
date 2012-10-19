e286222ef1d46fb3dd27769d97171de66e3af80c
Donald Curtis <dcurtis@milkbox.net>
2012-10-18 22:37:14 -0500

    configure the domain properly

--- config.py
+++ config.py
@@ -10,6 +10,6 @@ TWITTER_ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
 TWITTER_CONSUMER_KEY = 'vpYayPLKb1jeTb90KO14pQ'
 TWITTER_CONSUMER_SECRET = 'hnJRDSzY1JGCdfmvUxoaEsjdTw8vLDOasAuQkZi1J4'
 
-SITE_URL='http://mulchn.herokuapp.com'
+SITE_URL='http://mulchn.com'
 
 
