e80e3b7560ef8997acd94f5d846c79e296dd905b
Donald Curtis <dcurtis@milkbox.net>
2013-01-03 01:26:15 -0600

    update the dump script

--- scripts/heroku_dump.sh
+++ scripts/heroku_dump.sh
@@ -1,1 +1,3 @@
+mongodump --collection users --host alex.mongohq.com --port 10088 --db app8369946 -u backup -pwa7sy7phe8rtoh1fa6lu -o ./users
+mongodump --collection questions --host alex.mongohq.com --port 10088 --db app8369946 -u backup -pwa7sy7phe8rtoh1fa6lu -o ./questions
 mongodump --host alex.mongohq.com --port 10088 --db app8369946 -u backup -pwa7sy7phe8rtoh1fa6lu -o ./
