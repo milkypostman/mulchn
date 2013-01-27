7cbb1a00a90bc3e150361d7bad62cc5ae4bb10e4
Donald Curtis <dcurtis@milkbox.net>
2013-01-27 12:39:22 -0600

    update resetdb script

--- scripts/resetdb.py
+++ scripts/resetdb.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python
 
+import os
 import os.path
+import sqlalchemy as sa
+
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from sqlalchemy import create_engine
 from flask import Config
-
 import db
-import os
 
-config = Config("")
-config.from_envvar('MULCHN_CONFIG')
 
-database_url = os.environ.get("DATABASE_URL", config['DATABASE_URL'])
-engine = create_engine(database_url, echo=True)
+db.Base.metadata.bind = sa.create_engine(os.environ['DATABASE_URL'])
+db.Base.metadata.drop_all()
+db.Base.metadata.create_all()
 
-with engine.begin() as conn:
-    db.Twitter.metadata.drop_all(conn)
-    db.Twitter.metadata.create_all(conn)
-    db.Account.metadata.drop_all(conn)
-    db.Account.metadata.create_all(conn)
+
+
